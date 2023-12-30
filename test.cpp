a2268393b8d7c54840aa74683e74fee99f1e911b
Darrin-Lin <darrin.de88@gmail.com>
2023-12-30 20:45:44 +0800

    edit test.cpp

--- test.cpp
+++ test.cpp
@@ -3,17 +3,6 @@ using namespace std;
 
 int main(){
     int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++) cin>>arr[i];
-
-    int maxSum = INT_MIN;
-    int currSum = 0;
-    for(int i=0;i<n;i++){
-        currSum += arr[i];
-        if(currSum<0) currSum = 0;
-        maxSum = max(maxSum,currSum);
-    }
-    cout<<maxSum<<endl;
+    cin >> n;
     return 0;
 }
